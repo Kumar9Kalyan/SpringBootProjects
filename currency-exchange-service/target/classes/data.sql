0b70839561237c4221406bbf52299cee8a7f00c7
Kumar Kalyan <158476352+Kumar9Kalyan@users.noreply.github.com>
2024-07-18 03:47:03 +0530

    Add files via upload
    Has built a currency-exchange microservice. Used JPA repository to fetch the exchange value and also used a Circuit Breaker Pattern, Resilience4j dependency in this micro-service

---
+++ currency-exchange-service/target/classes/data.sql
@@ -0,0 +1,4 @@
+insert into currency_exchange (currency_from,currency_to,conversion_multiple,environment)
+values ('EUR','INR',90,'');
+insert into currency_exchange (currency_from,currency_to,conversion_multiple,environment)
+values ('USD','INR',83,'');
